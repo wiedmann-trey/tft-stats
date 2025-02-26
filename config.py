7567057ab3b117aa41fe4de6b34eb4e96129d680
wiedmann-trey <treywiedmann@gmail.com>
2025-02-26 02:06:03 -0500

    create folders if the dont exist

--- config.py
+++ config.py
@@ -5,3 +5,15 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 api_key = os.getenv('API_KEY')
+
+p = config["dirs"]["data_dir"]
+if not os.path.exists(p):
+    os.makedirs(p)
+
+p = config["dirs"]["data_dir"]+"\\"+config["dirs"]["fresh_data_folder"]
+if not os.path.exists(p):
+    os.makedirs(p)
+
+p = config["dirs"]["data_dir"]+"\\"+config["dirs"]["processed_data_folder"]
+if not os.path.exists(p):
+    os.makedirs(p)
